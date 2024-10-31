11207b441f0a856765ca15c707538d4ac9261573
dezuF <zf4dspace@gmail.com>
2024-10-31 17:13:49 -0400

    oka

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -1,4 +1,10 @@
 "use client"
+
+import { columns } from "@/components/collections/CollectionColumns"
+import { DataTable } from "@/components/custom ui/DataTable"
+import { Button } from "@/components/ui/button";
+import { Separator } from "@radix-ui/react-separator";
+import { Plus } from "lucide-react";
 import { useEffect, useState } from "react"
 
 const Collections = () => {
@@ -17,13 +23,25 @@ const Collections = () => {
             console.log("[collections_GET]", err)
         }
     };
+    
     useEffect(() => {
         getCollections();
-      }, []);
-      console.log(collections)
+    }, []);
+
+    console.log(collections)
     return (
-        <div>Collections</div>
-    )
-}
+        <div className="px-10 py-5">
+            <div className="flex items-center justify-between">
+                <p className="text-heading2-bold">Collections</p>
+                <Button className="bg-blue-1 text-white">
+                    <Plus className="h-4 w-4 mr-2" />
+                    Create Collection
+                </Button>
+            </div>
+            <Separator className="bg-grey-1 my-4" />
+            <DataTable columns={columns} data={collections} searchKey="title"/>
+        </div>
+    );
+};
 
 export default Collections
--- app/api/collections/router.ts
+++ app/api/collections/router.ts
@@ -52,16 +52,3 @@ export const GET = async (req: NextRequest) => {
     return new NextResponse("Internal Server Error", { status: 500 })
   }
 }
-
-export const GET = async (req: NextRequest) => {
-  try {
-    await connectToDB()
-
-    const collections = await Collection.find().sort({ createdAt: "desc" })
-
-    return NextResponse.json(collections, { status: 200 })
-  } catch (err) {
-    console.log("[collections_GET]", err)
-    return new NextResponse("Internal Server Error", { status: 500 })
-  }
-}
