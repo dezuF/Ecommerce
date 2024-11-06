24b8c36a3e50038625ebe2ed4fa2b559a57b3134
dezuF <zf4dspace@gmail.com>
2024-11-05 23:27:24 -0400

    zzzz

---
+++ app/api/collections/[collectionId]/route.ts
@@ -0,0 +1,19 @@
+import Collection from "@/lib/models/Collection";
+import { connectToDB } from "@/lib/mongoDB";
+import { auth } from "@clerk/nextjs/server";
+import { NextRequest, NextResponse } from "next/server";
+
+export const DELETE = async (req: NextRequest, { params }: { params: {collectionId: string}}) => {
+    try {
+        const { userId } = auth()
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 })
+        }
+        await connectToDB()
+        await Collection.findByIdAndDelete(params.collectionId)
+        return new NextResponse("Collection is delete", {status:200})
+    } catch (err) {
+        console.log("[collectionId_DELETE]", err)
+        return new NextResponse("Internal error", { status: 500 })
+    }
+}
--- components/custom ui/Delete.tsx
+++ components/custom ui/Delete.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import { Button } from '../ui/button'
 import { Trash } from 'lucide-react'
@@ -17,26 +19,26 @@ import {
 const Delete = () => {
   return (
     <AlertDialog>
-    <AlertDialogTrigger>
-    <Button className='bg-red-1 text-white'>
-        <Trash className='h-4 w-4'/>
-    </Button>
-    </AlertDialogTrigger>
-    <AlertDialogContent>
-      <AlertDialogHeader>
-        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-        <AlertDialogDescription>
-          This action cannot be undone. This will permanently delete your account
-          and remove your data from our servers.
-        </AlertDialogDescription>
-      </AlertDialogHeader>
-      <AlertDialogFooter>
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
-        <AlertDialogAction>Continue</AlertDialogAction>
-      </AlertDialogFooter>
-    </AlertDialogContent>
-  </AlertDialog>
-  
+      <AlertDialogTrigger>
+        <Button className='bg-red-1 text-white'>
+          <Trash className='h-4 w-4' />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className='text-red-1'>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            collection.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction className='bg-red-1 text'>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+
   )
 }
 
